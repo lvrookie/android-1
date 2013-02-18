20d2ba4978b40cd6a579c51919722b46f34ac799
miroof <jeremy.hervy@gmail.com>
2013-02-18 14:27:56 +0100

    Updated : 	- Client.java

--- disponif/src/com/sims2013/disponif/client/Client.java
+++ disponif/src/com/sims2013/disponif/client/Client.java
@@ -4,45 +4,100 @@ import org.alexd.jsonrpc.JSONRPCClient;
 import org.alexd.jsonrpc.JSONRPCException;
 import org.alexd.jsonrpc.JSONRPCThreadedClient;
 import org.alexd.jsonrpc.JSONRPCParams.Versions;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.AsyncTask;
 
 public class Client {
 	
-	// API Path
-	private static String HOST = "http://disponif.darkserver.fr/server/api.php";
+	// Error constants
+	public static final String ERROR_STRING = "error";
+	public static final String ERROR_JSON_STRING = "jsonError";
+	
+	// Method constants
+	private static final String PING_METHOD = "ping";
+	private static final String LOGIN_METHOD = "userLogin";
+	
+	// Param constants
+	private static final String ACCESS_TOKEN_PARAM = "accessToken";
+	
+	// Result constants
+	private static final String TOKEN_RESULT = "token";
+	
+	// Json client object
+	private JSONRPCClient mJsonClient;
+	
+	// Receive listener
+	private onReceiveListener mListener;
 	
 	// Response listener interface
-	public interface onStringReceiveListener {
+	public interface onReceiveListener {
 		public void onPingReceive(String result);
+		public void onLogInTokenReceive(String token);
+	}
+	
+	// API Path
+	//	private static String HOST = "http://disponif.darkserver.fr/server/api.php";
+	
+	// Public constructor
+	public Client(String host) {
+		mJsonClient = JSONRPCClient.create(host, Versions.VERSION_2);
+		mJsonClient.setConnectionTimeout(2000);
+		mJsonClient.setSoTimeout(2000);
+	}
+	
+	// Listener setter
+	public void setListener(onReceiveListener listener) {
+		this.mListener = listener;
 	}
 	
 	// Ping method
-	public static void ping(final Client.onStringReceiveListener listener) {
-		final String method = "ping";
-		
+	public void ping() {		
 		new AsyncTask<String, Void, String>(){
 
 			@Override
 			protected String doInBackground(String... params) {
-				JSONRPCClient client = JSONRPCThreadedClient.create(HOST, Versions.VERSION_2);
-				client.setConnectionTimeout(2000);
-		        client.setSoTimeout(2000);
 		        try {
-		        	String res = client.callString(method);
+		        	String res = mJsonClient.callString(PING_METHOD);
 		        	return res;
 		        } catch (JSONRPCException e) {
-		            return "error";
+		            return ERROR_STRING;
 		        }
 			}
 
 			@Override
 			protected void onPostExecute(String result) {
-				listener.onPingReceive(result);
+				mListener.onPingReceive(result);
 				super.onPostExecute(result);
 			}
 		}.execute();
-		
 	}
+	
+	//LogIn method
+	public void logIn(final String token) {
+		new AsyncTask<String, Void, String>(){
 
+			@Override
+			protected String doInBackground(String... params) {
+		        try {
+		        	JSONObject JSObjet = new JSONObject();
+		        	JSObjet.put(ACCESS_TOKEN_PARAM, token);
+		        	JSONObject res = mJsonClient.callJSONObject(LOGIN_METHOD, JSObjet);
+		        	return res.getString(TOKEN_RESULT);
+		        } catch (JSONRPCException e) {
+		            return ERROR_STRING;
+		        } catch (JSONException e) {
+		        	return ERROR_JSON_STRING;
+		        }
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				mListener.onLogInTokenReceive(result);
+				super.onPostExecute(result);
+			}
+		}.execute();
+	}
 }
